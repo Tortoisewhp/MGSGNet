535a545bdeecdadf9e8d5cc1b24234749721304b
Tortoisewhp <59301544+Tortoisewhp@users.noreply.github.com>
2024-05-18 20:38:31 +0800

    Update CMR.py

--- kd_loss/CMR.py
+++ kd_loss/CMR.py
@@ -13,9 +13,10 @@ def CMR(featureX,featureY):
 if __name__ == '__main__':
     r1_1 = torch.randn(1, 320, 13 ,13)
     r1_2 = torch.randn(1, 320, 13 ,13)
+    r1_3 = torch.randn(1, 320, 13 ,13)
     r2_1 = torch.randn(1, 320, 13 ,13)
     r2_2 = torch.randn(1, 320, 13 ,13)
-    cmr1=CMR(r1_1,r1_2)
+    cmr1=CMR(r1_1,r1_2+r1_3)
     cmr2=CMR(r2_1,r2_2)
     loss = nn.MSELoss(reduction='mean')
     print('loss:',loss(cmr1,cmr2))
